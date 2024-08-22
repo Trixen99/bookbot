229f36c1074e204e15128021bd5c9fd151f9f6e8
Trixen99 <NathanWhitehead99@hotmail.com>
2024-08-23 00:23:38 +0100

    added character counter function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     book_file_path = "books/frankenstein.txt"
     text = get_text(book_file_path)
+    characters_countdict = charactercount(text)
     print(text)
-    print(f"Word Count: {wordcount(text)}")
+    print(f"\nWord Count of book: {wordcount(text)}")
+    print("\nBelow are the characters used in order from most used to least used:")
+    for c in characters_countdict:
+        print(f"{c} = {characters_countdict[c]}")
+
 
 def get_text(path):
     with open(path) as text:
@@ -16,6 +21,23 @@ def wordcount(text):
     for word in to_be_counted:
         current_count += 1
     return current_count
+
+
+def charactercount(booktext):
+    characters = {}
+    booktext = booktext.lower()
+    booktext = booktext.split()
+    for words in booktext:
+        for letters in words:
+            if letters in characters:
+                characters[letters] += 1
+            else:
+                characters[letters] = 1
+    return (dict(sorted(characters.items(), key=lambda x: x[1], reverse=True)))
+
+            
+
+
 
         
 
